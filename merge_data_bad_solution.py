040fde37fe6eed11b6d1f78386d591c8a7f590f9
Riccardo Bruè <riccardo.brue@gmail.com>
2018-06-29 01:21:43 +0200

    Merger script done

--- merge_data_bad_solution.py
+++ merge_data_bad_solution.py
@@ -1,3 +1,4 @@
+from openpyxl import load_workbook
 import pandas as pd
 import numpy as np
 import os, os.path
@@ -35,14 +36,7 @@ def get_data(restore=False):
         # SELECT THE RELEVANT SIMULATION RESULT FILES
         # ----------------------------------------------------------
         list = os.listdir(simulation_folder_path)  # dir is your directory path
-
-        # ----------------------------------------------------------
-        # MERGE EXCEL STRUCTURE WITH SIMULATION DATA
-        # ----------------------------------------------------------
-
-        dictionary = {'R0': [], '%AGGR': [], 'HET': [], 'HOM ENERGY': [], 'HOM RATE': [], 'REECHD FND': [],
-                      'REECHD HND': [], 'HEED FND': [], 'HEED HND': [],
-                      'ERHEED FND': [], 'ERHEED HND': [], 'FMUC FND': [], 'FMUC HND': []}
+        selected_files = []
 
         for i in range(0, len(list)):
             current_file_name = list[i]
@@ -60,67 +54,106 @@ def get_data(restore=False):
 
             width = width[1:]
             height = height[1:]
+
             hom_energy = hom_energy[7:]
+            if hom_energy == "0.75":
+                hom_energy = 0.5
+            elif hom_energy == "0.5":
+                hom_energy = 0.3
+
             hom_rate = hom_rate[5:]
+
             perc_aggr = perc_aggr[3:]
             if perc_aggr == "100percentAggregation":
                 perc_aggr = 1
             elif perc_aggr == "70percentAggregation":
-                perc_aggr = 0.7
+                perc_aggr = 0.6
             elif perc_aggr == "40percentAggregation":
-                perc_aggr = 0.4
+                perc_aggr = 0.3
             elif perc_aggr == "NoAggregation":
                 perc_aggr = 0
+
             heterogeneity = heterogeneity[5:]
+            if heterogeneity == "0.7":
+                heterogeneity = 0.6
+            elif heterogeneity == "0.4":
+                heterogeneity = 0.3
+
             transm_range, extension = str(transm_range_e[2:]).split(".")
 
             if width == selected_width and height == selected_height:
+                selected_files.append(selected_files)
+                print(len(selected_files), ") ", current_file_name, " - ", hom_energy, ", ", hom_rate, ", ", perc_aggr,
+                      ", ", heterogeneity, ", ", transm_range)
 
-                print(current_file_name, " - ", hom_energy, ", ", hom_rate, ", ", perc_aggr, ", ", heterogeneity, ", ",
-                      transm_range)
+        # ----------------------------------------------------------
+        # LOAD THE RELEVANT EXCEL FILE
+        # ----------------------------------------------------------
+        excel_file = excel_folder_path + input_file
+        wb = load_workbook(excel_file, data_only=True)  # , read_only=True)
 
+        # selecting the first (or active) sheet
+        ws = wb.active
+        # or
+        # first_sheet = wb.get_sheet_names()[0]
+        # ws = wb.get_sheet_by_name(first_sheet)
 
-                dictionary.get("R0").append(transm_range)
-                dictionary.get("%AGGR").append(perc_aggr)
-                dictionary.get("HET").append(heterogeneity)
-                dictionary.get("HOM ENERGY").append(hom_energy)
-                dictionary.get("HOM RATE").append(hom_rate)
+        columns = np.array(['A', 'B', 'C', 'D', 'E'])  # is the first output column
+        num_attributes = 5
 
-                simulation_dataframe = pd.read_csv(simulation_folder_path + current_file_name, header=None)
+        data_dictionary = {}
+        max_rows = 0  # the first column defines the maximum number of rows
+        for c in range(len(columns)):
+            column = ws[columns[c]]
+            column_header = str(column[0].value)
+            data_dictionary.update({column_header: []})
 
-                REECHD_FND, REECHD_HND = str(simulation_dataframe.iat[1, 0]).split(";")
-                HEED_FND, HEED_HND = str(simulation_dataframe.iat[3, 0]).split(";")
-                ERHEED_FND, ERHEED_HND = str(simulation_dataframe.iat[5, 0]).split(";")
-                FMUC_FND, FMUC_HND = str(simulation_dataframe.iat[7, 0]).split(";")
+            for x in range(1, len(column)):
+                if column[x].value is None:
+                    if c == 0:
+                        max_rows = x
+                        break
+                    elif c > 0 and x < max_rows:
+                        data_dictionary[column_header].append(column[x].value)
+                    else:
+                        break
+                else:
+                    data_dictionary[column_header].append(column[x].value)
+        dataframe = pd.DataFrame(data=data_dictionary, columns=data_dictionary.keys())
 
-                dictionary.get("REECHD FND").append(REECHD_FND)
-                dictionary.get("REECHD HND").append(REECHD_HND)
-                dictionary.get("HEED FND").append(HEED_FND)
-                dictionary.get("HEED HND").append(HEED_HND)
-                dictionary.get("ERHEED FND").append(ERHEED_FND)
-                dictionary.get("ERHEED HND").append(ERHEED_HND)
-                dictionary.get("FMUC FND").append(FMUC_FND)
-                dictionary.get("FMUC HND").append(FMUC_HND)
+        # ----------------------------------------------------------
+        # MERGE EXCEL STRUCTURE WITH SIMULATION DATA
+        # ----------------------------------------------------------
 
-        """
-        print(len(dictionary.get("R0")),
-              len(dictionary.get("%AGGR")),
-              len(dictionary.get("HET")),
-              len(dictionary.get("HOM ENERGY")),
-              len(dictionary.get("HOM RATE")),
-              len(dictionary.get("REECHD FND")),
-              len(dictionary.get("REECHD HND")),
-              len(dictionary.get("HEED FND")),
-              len(dictionary.get("HEED HND")),
-              len(dictionary.get("ERHEED FND")),
-              len(dictionary.get("ERHEED HND")),
-              len(dictionary.get("FMUC FND")),
-              len(dictionary.get("FMUC HND"))
-              )
-        """
+        # add 4 empty columns for the protocol results (REECHD, HEED, ERHEED, FMUC)
+        dataframe["REECHD"] = None
+        dataframe["HEED"] = None
+        dataframe["ERHEED"] = None
+        dataframe["FMUC"] = None
 
-        dataframe = pd.DataFrame(data=dictionary, columns=dictionary.keys())
         print(dataframe)
+
+        for index, row in dataframe.iterrows():
+            hom_energy = row['HOM ENERGY']
+            hom_rate = row['HOM RATE ']
+            perc_aggr = row['%AGGR']
+            heterogeneity = row['HET']
+            transm_range = row['R0']
+
+            print(hom_energy, hom_rate, perc_aggr, heterogeneity, transm_range)
+
+        """
+        #print(dataframe)
+        # ----------------------------------------------------------
+        # MANIPULATING DATA
+        # ----------------------------------------------------------
+        dataframe['HEED FND'] = dataframe.apply(
+            lambda row: row['R0'] * row['HOM ENERGY'],
+            # lambda row: row['R0']*row['HOM ENERGY'] if np.isnan(row['c']) else row['c'],
+            axis=1
+        )
+        """
+        # print(dataframe)
 
         # ----------------------------------------------------------
         # STORING DATA TO A SIMPLEST CSV
